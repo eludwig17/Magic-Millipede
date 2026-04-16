2aeefcebfd58ecddb32d480412fddc39df8db09f
eludwig17 <elijahludwigyt@gmail.com>
2026-04-15 21:02:04 -0700

    included button pinout for the interior red LED, separated names between exterior & interior

--- pico-Millipede.ino
+++ pico-Millipede.ino
@@ -7,23 +7,29 @@
 
 // GPIO Pin Layout for Raspi Pico 2 W
 // pinout may need to be changed based on soldering location on perf board
-#define LED_R   5   
-#define LED_G   6   
-#define BTN     2  
+#define LED_INTERIOR_RED 4
+#define LED_EXTERIOR_RED 5
+#define LED_EXTERIOR_GREEN 6
+#define BTN 2
 
 void setup() {
   Serial.begin(9600);
-
-  pinMode(LED_R, OUTPUT);
-  pinMode(LED_G, OUTPUT);
+  
+  pinMode(LED_INTERIOR_RED, OUTPUT);
+  pinMode(LED_EXTERIOR_RED, OUTPUT);
+  pinMode(LED_EXTERIOR_GREEN, OUTPUT);
   pinMode(BTN, INPUT_PULLUP);
 
-  digitalWrite(LED_R, LOW);
-  digitalWrite(LED_G, LOW);
-
+  // setting all LEDS to LOW(OFF) state until its finished idling/device booted and running state
+  digitalWrite(LED_INTERIOR_RED, LOW);
+  digitalWrite(LED_EXTERIOR_RED, LOW);
+  digitalWrite(LED_EXTERIOR_GREEN, LOW);
+  
+  // after short idle state sets appropriate LEDS to HIGH(ON)
   delay(2000);
   
-  digitalWrite(LED_G, HIGH);
+  digitalWrite(LED_EXTERIOR_GREEN, HIGH);
+  digitalWrite(LED_INTERIOR_RED, HIGH);
 }
 
 void loop() {
@@ -31,8 +37,8 @@ void loop() {
   // button logic in testing
   if (digitalRead(BTN) == LOW) {
     delay(200);  
-    digitalWrite(LED_G, LOW);
-    digitalWrite(LED_R, HIGH);  
+    digitalWrite(LED_EXTERIOR_GREEN, LOW);
+    digitalWrite(LED_EXTERIOR_RED, HIGH);  
 
   }
 }
